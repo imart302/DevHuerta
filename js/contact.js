17a4ccae73ae563d984d6baf1c44df50ad503f9b
antonio <antoniolm386@gmail.com>
2023-10-03 22:17:48 -0600

    inputs de bootstrap agregados y validaciones individuales

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,35 @@
-
+const nameInput = document.getElementById('nameInput');
+const phoneInput = document.getElementById('phoneInput');
+const emailInput = document.getElementById('emailInput');
+const messageInput = document.getElementById('messageInput');
 const formBtn = document.getElementById('contactForm');
 
-/**
- * Función para verificar el evento submit del formulario
- */
-function testEvent(){
-    formBtn.addEventListener("submit", (e) => {
-        e.preventDefault();
-        console.log('Formulario enviado');
-    });
+/**Si el -imput- esta vacio, agrega la clase de bootstrap is-invalid, si ya esta completo, la quita */
+const isFilled = input => {
+    if(input.value == ''){
+        input.classList.add('is-invalid');
+    } else {
+        input.classList.remove('is-invalid');
+    };
 };
 
+/**Si el email es invalido, agrega la clase de bootstrap is-invalid, si es valido, la quita */
+const emailValidation = email => {
+    const emailPattern = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ;
+    if( !emailPattern.test(email.value) ){
+        email.classList.add('is-invalid');
+    } else {
+        email.classList.remove('is-invalid'); 
+    };
+};
+
+
+/**Eventos de los inputs en tiempo real */
+
+nameInput.addEventListener('blur', () => isFilled(nameInput));
+
+phoneInput.addEventListener('blur', () => isFilled(phoneInput));
+
+emailInput.addEventListener('blur', () => emailValidation(emailInput));
+
+messageInput.addEventListener('blur', () => isFilled(messageInput));
