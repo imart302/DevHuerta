5bde9c9b83cacfb28c74f09d2551f51eea9b001c
Ivan Martinez <imartinezchin@gmail.com>
2023-11-07 17:40:28 -0600

    feat(env): add env and fetchs auth (#92)

--- src/js/api/auth.js
+++ src/js/api/auth.js
@@ -1,28 +1,64 @@
-import { CreateUserDto } from "./dtos/createUser.js";
-import {LoggedUser} from "./dtos/loggedUser.js"
+import { CreateUserDto } from './dtos/createUser.js';
+import { LoggedUser } from './dtos/loggedUser.js';
+import { BE_URL } from '../utils/constants.js';
 
 /**
  * Funciones para llamar a todos los recursos
  * de la API de login o Register
  */
 
-
-const userLoggedFake = new LoggedUser(123,"Alberto","Diaz","albertodiaz@hotmail.com","asdfgh");
+const userLoggedFake = new LoggedUser(
+  123,
+  'Alberto',
+  'Diaz',
+  'albertodiaz@hotmail.com',
+  'asdfgh'
+);
 
 /**
  * Funcion asincrona para llamar a POST /api/auth/login
- * @param {*} userData 
+ * @param {*} userData
  */
-export async function login(userData){
-	userLoggedFake.email = userData.email;
-	return userLoggedFake;
+export async function login(userData) {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  const raw = JSON.stringify(userData);
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+  };
+
+
+  const resp = await fetch(`${BE_URL}/auth/login`, requestOptions);
+  const json = await resp.json();
+
+  return json;
 }
 
 /**
- * 
- * @param {CreateUserDto} userData 
- * @returns 
+ *
+ * @param {CreateUserDto} userData
+ * @returns
  */
-export async function createUser(userData){
-  return true;
+export async function createUser(userData) {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  const raw = JSON.stringify(userData);
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+  };
+
+  const response = await fetch(`${BE_URL}/auth/register`, requestOptions);
+  const json = await response.json();
+
+  return json;
 }
